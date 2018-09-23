2a06859afe450e3ad1940a40bd878f5a436bdda0
rananotfound <ashutosh.rana586@gmail.com>
2018-09-23 22:08:09 +0530

    Third Commit

--- app/src/main/java/com/ashutoshrana/calculator/MainActivity.java
+++ app/src/main/java/com/ashutoshrana/calculator/MainActivity.java
@@ -2,12 +2,18 @@ package com.ashutoshrana.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+TextView input,output;
+Button one,two,three,four,five,six,seven,eight,nine,zero,dot,plus,minus,multiple,divide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
